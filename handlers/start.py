0afc15eb96cb9f42d61c2f8ea563a92ff0b88620
Timofey <tima.rus-2007@mail.ru>
2023-12-03 14:19:08 +0500

    create subscriptions page

--- handlers/start.py
+++ handlers/start.py
@@ -32,19 +32,23 @@ async def process_callback(callback_query: types.CallbackQuery):
     chat_id = callback_query.message.chat.id
 
     if button_data == 'my_subscriptions':
-        await show_subscriptions(chat_id, callback_query.message.message_id)
+        await show_subscriptions(callback_query)
     elif button_data == 'anime_list':
         await show_anime_list(chat_id, callback_query.message.message_id)
     elif button_data == 'what_to_watch':
         await bot.edit_message_text('Посмотрите аниме', chat_id, callback_query.message.message_id)
 
 
-async def show_subscriptions(chat_id, message_id):
-    subscriptions = ['Подписка 1', 'Подписка 2', 'Подписка 3', 'Подписка 4', 'Подписка 5', 'Подписка 6']
-    subscriptions_per_row = 2
-    markup = subscription_keyboard(subscriptions, subscriptions_per_row)
+async def show_subscriptions(callback_query: types.CallbackQuery, page=1):
+    chat_id = callback_query.message.chat.id
+    message_id = callback_query.message.message_id
+    user_id = callback_query.from_user.id
 
-    await bot.edit_message_text('Ваши подписки:', chat_id, message_id, reply_markup=markup)
+    subscriptions = await subscriptions_db.select_all_subscriptions(user_id=user_id)
+    if not subscriptions:
+        return await bot.edit_message_text('У вас нет еще не одной активной подписки.', chat_id, message_id)
+    markup = await subscription_keyboard(subscriptions, user_id=user_id, page=page)
+    await bot.edit_message_text('Ваши активные подписки:', chat_id, message_id, reply_markup=markup)
 
 
 async def process_subscription(callback_query: types.CallbackQuery):
@@ -54,21 +58,34 @@ async def process_subscription(callback_query: types.CallbackQuery):
     markup = InlineKeyboardMarkup()
     markup.add(InlineKeyboardButton('На главную', callback_data='back'))
     markup.add(InlineKeyboardButton('Отменить подписку', callback_data=f'unsubscribe_{subscription_name}'))
-
-    await bot.edit_message_text(f'Выбрана подписка: {subscription_name}', chat_id, callback_query.message.message_id,
+    if callback_query.data == 'subscription_all':
+        return await bot.edit_message_text(f'Выбраны все аниме, вы уверены, что хотите их отменить?', chat_id,
+                                           callback_query.message.message_id,
+                                           reply_markup=markup)
+    anime_title = check_startswith(subscription_name).get('title')
+    await bot.edit_message_text(f'Выбрана подписка аниме: {anime_title}', chat_id, callback_query.message.message_id,
                                 reply_markup=markup)
 
 
 async def process_unsubscribe(callback_query: types.CallbackQuery):
-    subscription_name = callback_query.data.replace('unsubscribe_', '')
+    data = callback_query.data
     chat_id = callback_query.message.chat.id
 
-    await bot.edit_message_text('Подписка отменена', chat_id, callback_query.message.message_id)
+    if data == 'unsubscribe_all':
+        await subscriptions_db.delete_subscription(user_id=callback_query.from_user.id)
+        message = 'Вы успешно отписались от всех аниме.'
+        return await bot.edit_message_text(message, chat_id, callback_query.message.message_id)
+
+    subscription_name = data.replace('unsubscribe_', '')
+    anime_title = check_startswith(subscription_name).get('title')
+    await subscriptions_db.delete_subscription(anime_title=anime_title, user_id=callback_query.from_user.id)
+    message = 'Подписка отменена. Вам больше не будут приходить уведомления.'
+    await bot.edit_message_text(message, chat_id, callback_query.message.message_id)
 
 
 async def show_anime_list(chat_id, message_id, page=1):
     markup = await anime_keyboard(page=page)
-    await bot.edit_message_text('Список аниме:', chat_id, message_id, reply_markup=markup)
+    await bot.edit_message_text('Список всех аниме, которые сейчас выходят:', chat_id, message_id, reply_markup=markup)
 
 
 async def show_anime(callback_query: types.CallbackQuery):
@@ -115,12 +132,22 @@ async def anime_pagination_prev_page(callback_query: types.CallbackQuery):
     await show_anime_list(callback_query.message.chat.id, callback_query.message.message_id, page - 1)
 
 
+async def subs_pagination_next_page(callback_query: types.CallbackQuery):
+    page = int(callback_query.data.replace('next_subs_page_', ''))
+    await show_subscriptions(callback_query, page + 1)
+
+
+async def subs_pagination_prev_page(callback_query: types.CallbackQuery):
+    page = int(callback_query.data.replace('prev_subs_page_', ''))
+    await show_subscriptions(callback_query, page - 1)
+
+
 async def process_back(callback_query: types.CallbackQuery):
     return await start(callback_query)
 
 
 def register_start(dp: Dispatcher):
-    dp.register_message_handler(start, commands=['test'])
+    dp.register_message_handler(start, commands=['start'])
     dp.register_callback_query_handler(process_callback,
                                        lambda c: c.data in ['my_subscriptions', 'anime_list', 'what_to_watch'])
     dp.register_callback_query_handler(process_subscription,
@@ -135,5 +162,9 @@ def register_start(dp: Dispatcher):
                                        lambda c: c.data.startswith('next_anime_page_'))
     dp.register_callback_query_handler(anime_pagination_prev_page,
                                        lambda c: c.data.startswith('prev_anime_page_'))
+    dp.register_callback_query_handler(subs_pagination_next_page,
+                                       lambda c: c.data.startswith('next_subs_page_'))
+    dp.register_callback_query_handler(subs_pagination_prev_page,
+                                       lambda c: c.data.startswith('prev_subs_page_'))
     dp.register_callback_query_handler(process_back,
                                        lambda c: c.data == 'back')
--- keyboards/subscription_keyboard.py
+++ keyboards/subscription_keyboard.py
@@ -1,23 +1,46 @@
+from math import ceil
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+from data.config import subscriptions_db
 
-def subscription_keyboard(subscriptions: list, subscriptions_per_row: int, page: int = 0) -> InlineKeyboardMarkup:
-    subscription_chunks = [subscriptions[i:i + subscriptions_per_row] for i in
-                           range(0, len(subscriptions), subscriptions_per_row)]
+
+async def subscription_keyboard(subscriptions: list, user_id: int, subscriptions_per_row: int = 2, page: int = 1,
+                                offset: int = 6) -> InlineKeyboardMarkup:
+    subscriptions_list = [sub.get('anime_title') for sub in subscriptions]
+    total_pages = ceil(len(subscriptions_list) / offset)
+
+    if page <= 0:
+        page = total_pages
+    if page > total_pages:
+        page = 1
+
+    start_index = (page - 1) * offset
+    end_index = start_index + offset
+    current_list = subscriptions_list[start_index:end_index]
+
+    subscriptions_chunks = [current_list[i:i + subscriptions_per_row] for i in
+                            range(0, len(current_list), subscriptions_per_row)]
     markup = InlineKeyboardMarkup(row_width=subscriptions_per_row + 1)
 
-    markup.add(InlineKeyboardButton("Отменить все", callback_data='cancel_subscriptions'))
-    for row in subscription_chunks:
-        buttons = [InlineKeyboardButton(subscription, callback_data=f'subscription_{subscription}') for i, subscription
-                   in
-                   enumerate(row)]
+    markup.add(InlineKeyboardButton("Отписаться от всего", callback_data='subscription_all'))
+    for row in subscriptions_chunks:
+        buttons = [
+            InlineKeyboardButton(
+                text=f'{sub[0: 25]}...',
+                callback_data=f'subscription_{sub[0: 20]}')
+            for i, sub in enumerate(row)
+        ]
         markup.add(*buttons)
 
-    markup.add(*[
-        InlineKeyboardButton("<-", callback_data='subscription_prev_page'),
-        InlineKeyboardButton(str(page), callback_data=f'subscription_page_{page}'),
-        InlineKeyboardButton("->", callback_data='subscription_next_page')
-    ])
+    pagination_buttons = [
+        InlineKeyboardButton(f'{page}/{total_pages}', callback_data=f'page_subs_{page}'),
+    ]
+
+    if total_pages > 1:
+        pagination_buttons.insert(0, InlineKeyboardButton("<-", callback_data=f'prev_subs_page_{page}'))
+        pagination_buttons.append(InlineKeyboardButton("->", callback_data=f'next_subs_page_{page}'))
+
+    markup.add(*pagination_buttons)
     markup.add(InlineKeyboardButton("На главную", callback_data='back'))
 
     return markup
